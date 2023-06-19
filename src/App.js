04eee19b8f504658a13032d50a24ec12b2ae3616
Karimah Nichols <ms.karimah.nichols@gmail.com>
2023-06-19 14:12:47 -0400

    add initial browser router and page folders for Farms. components completed outside of local repo not yet imported.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Browserrouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Browserrouter>
+      <Routes>
+        <Route path="/" element={<FarmSearchPage/>} />
+        <Route path="farms" element={<FarmCategoryPage/>} />
+        <Route path="farms/:id" element={<SelectedFarmPage/>} />
+      </Routes>
+  
+    </Browserrouter>
   );
 }
 
