85b9d731abe43343ae0936aeebf8d463f81f13ba
Karimah Nichols <ms.karimah.nichols@gmail.com>
2023-06-25 23:40:46 -0400

    style additional categories on selectedfarm page

--- src/Components/FarmDetails/FarmDetails.js
+++ src/Components/FarmDetails/FarmDetails.js
@@ -7,8 +7,9 @@ export default function FarmDetails({ farm }) {
         <main className="details">
 
             <div className='details__description'>
-                {farm.description}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <h3 className='details__header'>Description of program:</h3>
+                <p className=''></p>{farm.description}
+
             </div>
             <div className="details__location">
                 <h3 className="details__text details__text--region">
@@ -21,32 +22,30 @@ export default function FarmDetails({ farm }) {
 
             <div className='details__table'>
                 <div className='details__table-row'>
-                    <h3 className='details__table-row'>Type of Farm:</h3>
+                    <h3 className='details__row-header'>Type of Farm:</h3>
                     <div className='details__table-row--regular'>
                         {farm.farmType.includes('fruitVegetable') &&
-                            <h4 className='details__farm-type'>Fruit and Vegetable</h4>}
+                            <p className='details__farm-type'>Fruit and Vegetable</p>}
 
                         {farm.farmType.includes('flowerHerb') &&
-                            <h4 className='details__farm-type'>Flower and Herb</h4>}
+                            <p className='details__farm-type'>Flower and Herb</p>}
 
                         {farm.farmType.includes('aquaponicFish') &&
-                            <h4 className='details__farm-type'>Hydroponic/Aquaponic/Aeroponic or Fish</h4>}
-                        
-                        {farm.farmType.includes('gardenSmallSpaceRooftopUrban') &&
-                            <h4 className='details__farm-type'>Garden, Small-space, Rooftop, or Urban</h4>}
-                        
-                        {farm.farmType.includes('beePollinatorInsect') &&
-                            <h4 lassName='details__farm-type'>Bee, Pollinator, or Insect</h4>}
-                                                    
-                        {farm.farmType.includes('poultryLivestock') &&
-                            <h4 lassName='details__farm-type'>Poultry or Livestock</h4>}
-                                                    
-                        {farm.farmType.includes('indoorGreenhouse') &&
-                            <h4 lassName='details__farm-type'>Indoor or Greenhouse</h4>}
+                            <p className='details__farm-type'>Hydroponic/Aquaponic/Aeroponic or Fish</p>}
 
+                        {farm.farmType.includes('gardenSmallSpaceRooftopUrban') &&
+                            <p className='details__farm-type'>Garden, Small-space, Rooftop, or Urban</p>}
+
+                        {farm.farmType.includes('beePollinatorInsect') &&
+                            <p className='details__farm-type'>Bee, Pollinator, or Insect</p>}
+
+                        {farm.farmType.includes('poultryLivestock') &&
+                            <p className='details__farm-type'>Poultry or Livestock</p>}
+
+                        {farm.farmType.includes('indoorGreenhouse') &&
+                            <p className='details__farm-type'>Indoor or Greenhouse</p>}
 
                     </div>
-
 
                     {/* { farm.farmType.entries(value).forEach(([farmType, value]) => { return(`${value}`) }) }</h3> */}
 
@@ -54,42 +53,62 @@ export default function FarmDetails({ farm }) {
                      )} */}
 
                 </div>
-                <div className='details__table-row'>
+                <div className='details__table-row details__affirming'>
+                <h3 className='details__row-header'>Notes on Special Programming:</h3>
                     {farm.qtbipocAffirming &&
                         <div className="details__conditional-row">
                             <p className='details__table-text'>QTBIPOC Affirming </p>
                             <img className="details__checkbox" alt="checkbox badge icon" src={checkSrc} />
                         </div>
                     }
-                </div>
-                <div className='details__table-row'>
-                    {farm.blackAffirming &&
+                   
+                {/* </div>
+                <div className='details__table-row'> */}
+                     {farm.blackAffirming &&
                         <div className="details__conditional-row">
                             <p className='details__table-text'>Black Affirming </p>
                             <img className="details__checkbox" alt="checkbox badge icon" src={checkSrc} />
                         </div>
                     }
                 </div>
+
+
                 <div className='details__table-row'>
-                    {farm.hiresApprentices &&
+                    <h3 className='details__row-header'>Costs and Considerations:</h3>
+                    {farm.hiresApprentices ?
                         <div className="details__conditional-row">
-                            <p className='details__table-text'> Hires Apprentices</p>
+                            <p className='details__table-text'>Hires Apprentices (after program) </p>
                             <img className="details__checkbox" alt="checkbox badge icon" src={checkSrc} />
-                        </div>
-                    }
-                </div>
+                        </div> : 
+                        <p className='details__header-response'>Hires Apprentices (after program): no</p>
+                    } 
+                {/* </div>
 
 
-                <div className='details__table-row'>
+                <div className='details__table-row'> */}
                     {farm.paidApprenticeship &&
                         <div className="details__conditional-row">
-                            <p className='details__table-text'> Pays Apprenctices //</p>
+                            <p className='details__table-text'> Pays Apprenctices </p>
                             <img className="details__checkbox" alt="checkbox badge icon" src={checkSrc} />
                         </div>
                     }
                 </div>
 
 
+                <div className='details__table-row'>
+                    <p className='details__table-text'>Program Fees (if applicable): ${farm.programFee}</p>
+                </div>
+                <div className='details__table-row'>
+                    <p className='details__table-text'>Accessibility Accommodation notes: {farm.Accessibility}</p>
+                </div>
+                <div className='details__table-row'>
+                    {farm.lodgingAvailable &&
+                        <div className="details__conditional-row">
+                            <p className='details__table-text'>Housing / Lodging Available On Farm: </p>
+                            <div><img className="details__checkbox" alt="checkbox badge icon" src={checkSrc} /></div>
+                        </div>
+                    }
+                </div>
 
 
 
