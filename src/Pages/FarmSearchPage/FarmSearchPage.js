9c8c5f46dcd0ad7675f002cbdad1dcbce0ed4443
Karimah Nichols <ms.karimah.nichols@gmail.com>
2023-07-09 11:19:33 -0400

    add header to search page and background image of dandelion seeds

--- src/Pages/FarmSearchPage/FarmSearchPage.js
+++ src/Pages/FarmSearchPage/FarmSearchPage.js
@@ -8,37 +8,40 @@ export default function FarmSearchPage() {
     return (
         <div className='site-bg' style={{
             backgroundImage: `url(${backgroundImage})`,
-            backgroundRepeat: 'no-repeat', backgroundSize: 'cover' 
-            
+            backgroundRepeat: 'no-repeat', backgroundPositionY:'-50rem', backgroundSize: 'cover', backgroundPosition: 'center'
+
         }}>
-            <div className='browse'>
-                <section className='browse__section--farmtypes'>
+            <div className='browse-wrapper'>
+                <h1 className='browse__header'> Browse Farms By Category</h1>
+                <div className='browse'>
+                    <section className='browse__section--farmtypes'>
 
-                    <div className='browse__section--farmtypes-img'>
-                    </div>
+                        <div className='browse__section--farmtypes-img'>
+                        </div>
 
-                    <Link className='browse__link--farmtypes' to="/browse/farmtypes">
-                        <h2 className='browse__subheader'>Search by Type of Farm</h2>
-                    </Link>
-                </section>
-                <section className='browse__section--communities'>
+                        <Link className='browse__link--farmtypes' to="/browse/farmtypes">
+                            <h2 className='browse__subheader'>Search by Type of Farm</h2>
+                        </Link>
+                    </section>
+                    <section className='browse__section--communities'>
 
-                    <div className='browse__section--communities-img'>
-                    </div>
+                        <div className='browse__section--communities-img'>
+                        </div>
 
-                    <Link className='browse__link--communities' to="/browse/communities">
-                        <h2 className='browse__subheader'>Search by Community of Focus</h2>
-                    </Link>
-                </section>
-                <section className='browse__section--regions'>
+                        <Link className='browse__link--communities' to="/browse/communities">
+                            <h2 className='browse__subheader'>Search by Community of Focus</h2>
+                        </Link>
+                    </section>
+                    <section className='browse__section--regions'>
 
-                    <div className='browse__section--regions-img'>
-                    </div>
+                        <div className='browse__section--regions-img'>
+                        </div>
 
-                    <Link className='browse__link--regions' to="/browse/regions">
-                        <h2 className='browse__subheader'>Search by Region</h2>
-                    </Link>
-                </section>
+                        <Link className='browse__link--regions' to="/browse/regions">
+                            <h2 className='browse__subheader'>Search by Region</h2>
+                        </Link>
+                    </section>
+                </div>
             </div>
         </div>
     )
